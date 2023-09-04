94fcdeef2b1ab58b022bdf6ceb2d25c8030ba722
ghsparta <1093097279@qq.com>
2023-09-04 08:38:15 +0800

    week01

---
+++ src/main/java/week01/CharDemo.java
@@ -0,0 +1,22 @@
+package week01;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char c=0x6211;
+        char c1='我';
+        char[] codeUnits = new char[2];
+        codeUnits[0]=55356;
+        codeUnits[1]=57118;
+        System.out.println(codeUnits);
+    }
+}
